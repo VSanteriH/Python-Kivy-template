d96cc233a250e2c6769c397662481107976e1c49
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-13 15:30:34 +0200

    updated comments and made code bit more clear to read.

--- changecolor.py
+++ changecolor.py
@@ -1,27 +1,30 @@
-#Changes black pixels to white
-#This is usefull if we have black icons and want them to be white so icon color manupalation would be possible inside kv file.
 import os
-from PIL import Image #https://pillow.readthedocs.io/en/stable/
+from PIL import Image # https://pillow.readthedocs.io/en/stable/
 
-#Finds all png images in icons folder and chages those images black pixels to white
-def FindFiles():
-    directory = os.fsencode( "icons/")
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".png"): 
-            ChangeColor("icons/" + filename)
-            continue
-        else:
-            continue
-def ChangeColor(image):
+# Changes black pixels to white
+def change_color(image):
     newimage = Image.open(image)
-
+    # Goes trought every pixel from image and changes them to white
     for x in range(newimage.size[0]):
         for y in range(newimage.size[1]):
             r,g,b,a = newimage.getpixel((x,y))
-            if r == 0 and g == 0 and b == 0 and a > 0: #Changes black pixels to white
-                newimage.putpixel((x,y), (255,255,255,a)) 
-           # if r == 255 and g == 255 and b == 255 and a > 0: #Changes white pixels to black
-                #newimage.putpixel((x,y), (0,0,0,a)) 
+            if r == 0 and g == 0 and b == 0 and a > 0:
+               
+                newimage.putpixel((x,y), (255,255,255,a))
                 
-    newimage.save(image)#saves a file over the old one.
+    newimage.save(image) # Saves a file over the old one.
+
+def find_files():
+    # Gets folder wher icons are located
+    directory = os.fsencode( "icons/")
+
+    # Gets every file from folder
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+
+        # Gets png image files and calls ChangeColor
+        if filename.endswith(".png"): 
+            change_color("icons/" + filename)
+            continue
+        else:
+            continue
