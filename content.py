16fca7d3a2fa60b872d7e7b19643164385a03bae
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-13 15:48:31 +0200

    Moved popup to kv and made visual changes to main view.     - Changed colors of elements.     - Changed classes names.     - Cleared comments.

--- content.py
+++ content.py
@@ -1,13 +1,14 @@
 from kivy.core.window import Window
-from kivy.properties import StringProperty, ListProperty, NumericProperty
+from kivy.properties import StringProperty, ListProperty, NumericProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.storage.jsonstore import JsonStore
+from kivy.uix.widget import Widget
 import app
 import random
 
 from kivy.lang import Builder
-#from kivy.uix.widget import Widget
+
 def loadarticles(lenght ,articlesList):
     data1 = JsonStore('content.json')
     data = data1.get('rows')
@@ -22,58 +23,62 @@ def loadarticles(lenght ,articlesList):
             "text": text,
         } 
         oldList.append(article)
-    
     return oldList
 
-
+class FirstLoad():
+    first_load = True  
+    textcont = ObjectProperty
 
 class Article(BoxLayout):
-    label_title = StringProperty()
-    label_text = StringProperty()
+    title = StringProperty()
+    text = StringProperty()
     r = NumericProperty()
     g = NumericProperty()
     b = NumericProperty()
-    le = 0
-    sizeY = 140 #Sets height of article
+    sizeY = 140 # Sets height of article
 
-#Gets X ammount of Article classes and make 1 element of those
-class TextContent(BoxLayout):
+class TextContent(BoxLayout): 
     articlesList = []
     startpos = len(articlesList)
-    articles = loadarticles(startpos, articlesList) #Gets aricles from 0 to 10 and add those to oldList and returns that
+    articles = loadarticles(startpos, articlesList) # Gets aricles from 0 to 10 and add those to oldList and returns that
     articlesList = articles 
-
-    def updateList(title, text):
-        #super(TextContent, self).__init__(**kwargs)
-        lbl = Article()       
-        lbl.label_title = title
-        lbl.label_text = text
-        print(TextContent)
-       # TextContent.add_widget(lbl)#add articles to TextContent 
-        #TextContent.articlesList.append(lbl)
+    newtitle = StringProperty()
+    newtext = StringProperty()
+    
+    def update_list(self):
+        print("Add to List")
+        super(TextContent, self).__init__()
+        FirstLoad.textcont.orientation = "vertical" 
+        arti = Article()       
+        arti.title = self.newtitle
+        arti.text = self.newtext
         
-        #print( TextContent.articlesList)
-        pass
-
-    def doList(self, **kwargs):
-        print(self)
-        super(TextContent, self).__init__(**kwargs)
+        #FirstLoad.textcont.remove_widget(10)
+        FirstLoad.textcont.add_widget(arti,app.MainWindow.articleammount) # add articles to TextContent copy and local view
+        app.MainWindow.articleammount = app.MainWindow.articleammount + 1
+        print(app.MainWindow.articleammount)
+        app.MainApp().build()
+    
+    def do_list(self):
+        print("Do list")
+        super(TextContent, self).__init__()
         self.orientation = "vertical"
         for file in self.articlesList:
-            lbl = Article()       
-            lbl.label_title = file['title']
-            lbl.label_text = file['text']
-            lbl.r = round(random.uniform(.5, .7), 1)
-            lbl.b = round(random.uniform(.5, .9), 1)
-            lbl.g = round(random.uniform(.2, .8), 1)
+            arti = Article()       
+            arti.title = file['title']
+            arti.text = file['text']
+            
+            # Random colors for article background
+            arti.r = round(random.uniform(.5, .7), 1)
+            arti.b = round(random.uniform(.5, .9), 1)
+            arti.g = round(random.uniform(.2, .8), 1)
+            self.add_widget(arti) # add articles to TextContent  
 
-            print(lbl.r)
-
-            self.add_widget(lbl)#add articles to TextContent   
-        self.len = len(self.articlesList)
-       # app.MainWindow.SiteLenght(self,self.len)
-        
     def __init__(self, **kwargs):
-        self.doList()
-        
+        if FirstLoad.first_load == True:
+            self.do_list()
+            FirstLoad.first_load = False 
+            FirstLoad.textcont = self # Adds first TextContent class to textcont ObjectProperty so data can be added to list later
+        else:
+            self.update_list()
         
