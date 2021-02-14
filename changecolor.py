c473af0fca0f6ef8baa016d4eccea8345dec54c0
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-14 16:14:09 +0200

    Added description for file.

--- changecolor.py
+++ changecolor.py
@@ -1,18 +1,14 @@
+'''
+Change color
+==============
+
+This file is used for changing icons color from black to white, so icon color could be 
+changed in code.
+
+'''
+
 import os
 from PIL import Image # https://pillow.readthedocs.io/en/stable/
-
-# Changes black pixels to white
-def change_color(image):
-    newimage = Image.open(image)
-    # Goes trought every pixel from image and changes them to white
-    for x in range(newimage.size[0]):
-        for y in range(newimage.size[1]):
-            r,g,b,a = newimage.getpixel((x,y))
-            if r == 0 and g == 0 and b == 0 and a > 0:
-               
-                newimage.putpixel((x,y), (255,255,255,a))
-                
-    newimage.save(image) # Saves a file over the old one.
 
 def find_files():
     # Gets folder wher icons are located
@@ -28,3 +24,15 @@ def find_files():
             continue
         else:
             continue
+        
+# Changes black pixels to white
+def change_color(image):
+    newimage = Image.open(image)
+    # Goes trought every pixel from image and changes them to white
+    for x in range(newimage.size[0]):
+        for y in range(newimage.size[1]):
+            r,g,b,a = newimage.getpixel((x,y))
+            if r == 0 and g == 0 and b == 0 and a > 0: 
+                newimage.putpixel((x,y), (255,255,255,a))
+
+    newimage.save(image) # Saves a file over the old one.
