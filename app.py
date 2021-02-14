976bbb8d70ee4cac73969ddc73e7900a843f83d6
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-14 15:15:50 +0200

    Removed unnecessary variables and function     - TextContent class needs only article_ammount variable to work now.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import NumericProperty
 from kivy.core.window import Window
 
 # Size and position for testing
@@ -16,8 +17,7 @@ import changecolor # Change color of files in icons folder
 import content # Articless of the app(Mid section)
 
 class MainWindow(Screen):
-    articleammount = 10
-    articleheight = 140 
+    article_ammount = 10
 
 class SecondWindow(Screen):
     pass
@@ -43,6 +43,6 @@ class MainApp(App):
         content.TextContent() # Calls TextContent class whoms __init__ that calls updateList()
 
 if __name__ == "__main__":
-    # changecolor.find_files()   #Use this to change your projects black icons to white in icons folder.
+    # changecolor.find_files()   # Use this to change your projects black icons to white in icons folder.
     app = MainApp()
     app.run()
