62ebfa719c3d46e0ebe954127b35e396f0c1d7ee
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-14 19:54:55 +0200

    Added autoscroll to keep scroll position in place when loading more data.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import NumericProperty
 from kivy.core.window import Window
-
+from kivy.uix.camera import Camera
 # Size and position for testing
 Window.size = (360, 640) # Samsung s5 size
 Window.left = 1500 # Right side of screen
@@ -18,7 +18,11 @@ import content # Articless of the app(Mid section)
 
 class MainWindow(Screen):
     article_ammount = 10
-
+    def scrolls(self , scroll): # Called after scroll is relased
+        if(scroll.scroll_y <= 0.001): # When scroll position is at   
+            content.ArticlesContainer.load_more(self)
+            scroll.scroll_to(content.ArticlesContainerCopy.articles_container_copy.children[10] , padding=0, animate=False)
+ 
 class SecondWindow(Screen):
     pass
 
@@ -37,10 +41,10 @@ class MainApp(App):
 
     # Called from kv files popup button.
     def AddToList(self,title, text):
-        # Adds title and text to TextContent class StringObjects
-        content.TextContent.newtitle = title
-        content.TextContent.newtext = text
-        content.TextContent() # Calls TextContent class whoms __init__ that calls updateList()
+        # Adds title and text to ArticlesContainer class StringObjects
+        content.ArticlesContainer.newtitle = title
+        content.ArticlesContainer.newtext = text
+        content.ArticlesContainer() # Calls ArticlesContainer class whoms __init__ that calls updateList()
 
 if __name__ == "__main__":
     # changecolor.find_files()   # Use this to change your projects black icons to white in icons folder.
