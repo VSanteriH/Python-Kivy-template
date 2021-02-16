2369562e694c220e688bdc074f5ea0dbbe2f77f5
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-16 19:36:32 +0200

    Added clarifying comments

--- changecolor.py
+++ changecolor.py
@@ -11,28 +11,25 @@ import os
 from PIL import Image # https://pillow.readthedocs.io/en/stable/
 
 def find_files():
-    # Gets folder wher icons are located
-    directory = os.fsencode( "icons/")
-
-    # Gets every file from folder
-    for file in os.listdir(directory):
+    """ Finds all files from icons folder that name ends with .png """
+    directory = os.fsencode( "icons/") # Gets folder where icons are located
+ 
+    for file in os.listdir(directory): # Gets every file from folder
         filename = os.fsdecode(file)
-
-        # Gets png image files and calls ChangeColor
-        if filename.endswith(".png"): 
+        if filename.endswith(".png"):
             change_color("icons/" + filename)
             continue
         else:
             continue
         
-# Changes black pixels to white
 def change_color(image):
+    """ Changes every black pixel to white from image that was send to it. Skips transperent pixels """
     newimage = Image.open(image)
-    # Goes trought every pixel from image and changes them to white
-    for x in range(newimage.size[0]):
-        for y in range(newimage.size[1]):
-            r,g,b,a = newimage.getpixel((x,y))
-            if r == 0 and g == 0 and b == 0 and a > 0: 
-                newimage.putpixel((x,y), (255,255,255,a))
+    
+    for x in range(newimage.size[0]):# Goes trought every pixel of image in X axis
+        for y in range(newimage.size[1]): # In Y axis
+            r,g,b,a = newimage.getpixel((x,y)) # Get pixels color in rgb
+            if r == 0 and g == 0 and b == 0 and a > 0: # If pixel is black and not transparent.
+                newimage.putpixel((x,y), (255,255,255,a)) # Change color to white. Keep transperency.
 
     newimage.save(image) # Saves a file over the old one.
