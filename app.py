0bc52720e55b2fe2f154a349046365dae9682bd1
Santeri Hartikainen <v.s.hartikainen@gmail.com>
2021-02-16 19:34:04 +0200

    Fixed scroll_y position after data is loaded.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # Code by Santeri Hartikainen
 import kivy
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.lang import Builder
+from kivy.uix.modalview import ModalView
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 from kivy.properties import NumericProperty
 from kivy.core.window import Window
-
+import time
 # Size and position for testing
 Window.size = (360, 640) # Samsung s5 size
 Window.left = 1500 # Right side of screen
@@ -14,21 +18,19 @@ Window.left = 1500 # Right side of screen
 Window.clearcolor = (.94, 1, 1, 1)
 
 import changecolor # Change color of files in icons folder
-import content # Articless of the app(Mid section)
+import content # Articless of the app
 
 class MainWindow(Screen):
     article_ammount = 0
-    distance = 0.2 
-    def scrolls(self , scroll): # Called after scroll is started
-        print(scroll.scroll_y)
-        if(scroll.scroll_y <= MainWindow.distance): # When scroll position is at point we want to load more data   
-            print(scroll.scroll_y)
-            
+    distance = 0 
+    def scrolls(self , scroll):
+        """ Called every time scoll starts. Checks if we have reached bottom of the screen.
+        If so, calls ArticlesContainer.load_more in content.py that loads 1000 rows more.
+        After the load is done, scrolls to the previous scroll_y. 
+        """
+        if(scroll.scroll_y <= MainWindow.distance):
             content.ArticlesContainer.load_more(self)  
-            scroll.scroll_y = scroll.scroll_y + MainWindow.distance * 1.5 
-            MainWindow.distance = MainWindow.distance / 1.5 
-
-
+            scroll.scroll_to(content.ArticlesContainerCopy.articles_container_copy.children[1000] , padding=0, animate=True)
 
 class SecondWindow(Screen):
     pass
@@ -46,14 +48,14 @@ class MainApp(App):
     def build(self):   
         pass
 
-    # Called from kv files popup button.
     def AddToList(self,title, text):
-        # Adds title and text to ArticlesContainer class StringObjects
+        """ Called from kv file popup MainApp section. Gets title and text and add those to ArticleContainer class.
+        After that calls ArticleContainer class.
+        """
         content.ArticlesContainer.newtitle = title
         content.ArticlesContainer.newtext = text
         content.ArticlesContainer() # Calls ArticlesContainer class whoms __init__ that calls updateList()
 
 if __name__ == "__main__":
-    # changecolor.find_files()   # Use this to change your projects black icons to white in icons folder.
     app = MainApp()
     app.run()
